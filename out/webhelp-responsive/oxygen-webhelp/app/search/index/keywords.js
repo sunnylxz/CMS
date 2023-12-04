80811d3f5eb6628d4f02f3463c4334685f570b4d
sunnylxz <sunnyzlx0213@163.com>
2023-12-04 22:32:09 +0800

    首次导入数据”

---
+++ out/webhelp-responsive/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,8 @@
+var keywords=[{w:"context",p:["p0"]},{w:"sensitive",p:["p0"]},{w:"help",p:["p0"]},{w:"\u767B\u5F55",p:["p1"]},{w:"\u5F55\u5BA2",p:["p1"]},{w:"\u5BA2\u6237",p:["p1"]},{w:"\u6237\u7AEF",p:["p1"]}];
+var ph={};
+ph["p0"]=[0, 1, 2];
+ph["p1"]=[3, 4, 5, 6];
+var keywordsInfo = {
+    keywords: keywords,
+    ph: ph
+}
